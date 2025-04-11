28bfe0324863729ffde7a13aaf0594da5ab34f86
Nishit Shah <72440753+nishit-shah190@users.noreply.github.com>
2025-04-11 18:17:58 -0400

    Error fixed

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { Education } from "./components/Education/Education";
 import { Hero } from "./components/Hero/Hero";
 import { Navbar } from "./components/Navbar/Navbar";
 import { Projects } from "./components/Projects/Projects";
-import { Skills } from "./components/Skills/skills";
+import { Skills } from "./components/Skills/Skills";
 
 function App() {
   return (
